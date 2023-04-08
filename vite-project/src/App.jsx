09e346f9307c5572b2fb4f16cd8258b497602341
Santosh <sy59021321@student.ku.edu.np>
2023-04-08 17:08:54 +0545

    Made responsive!

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-export default function Signup()
-{
-  return(
-    <>
-       <h1>Sign Up</h1>
-       <div className="inputs">
+export default function Signup() {
+  return (
+    <div className="layout">
+      <h1>Sign Up</h1>
+      <form action="none">
+        <div className="input">
           <input type="text" placeholder="Full Name"></input>
           <input type="email" placeholder=" Email"></input>
         </div>
         <div className="input">
           <input type="password" placeholder="Password"></input>
           <input type="password" placeholder="Re-enter Password"></input>
-          <button type="submit">Sign Up</button>
         </div>
-
-        <div className="line"></div>
+        <button type="submit">Sign Up</button>
+      </form>
+      <div className="or">OR</div>
+      <div className="lines">
+        <div className="line1"></div>
         <div className="line2"></div>
-        <div className="or">OR</div>
-        
-       
-    </>
+      </div>
+    </div>
   );
 }
-
